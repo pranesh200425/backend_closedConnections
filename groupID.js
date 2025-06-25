c67c5cca9bceea3076ce36d6b97eae3f5081ee10
spectre007 <pranesh200425@gmail.com>
2025-06-25 17:30:16 +0530

    added groups for the feed algorithim. This is the most important component of this app

--- groupID.js
+++ groupID.js
@@ -5,26 +5,43 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
 
 const mongo = process.env.MONGO_URI;
 //console.log(mongo)
 
-const Group = New mongoose.model("Group", group);
+const groupschema = mongoose.Schema({
+  groupID: { type: Number, required: true },
+  members: { type: [Number] },
+});
+
+const Group = new mongoose.model("Group", groupschema);
 let gID = 0;
 app.post('/api/postGroup', async (req, res)=> {  
-  const  groupID = gID++ ;
+  let groupID = 0;
+  const members = [];
   try {
-  const Group = await Group.create({ groupID, members });
-}catch (err) {
+    for(let i = 0; i < 100; i++) {
+  const GroupDoc = await Group.create({ groupID, members });
+  groupID++;
+    }
+  res.status(201).json({message: "Group created successfully"});
+  
+}catch (err) { 
     res.status(500).json({ message: "Server error" });
   }
 })
 
-const group = mongoose.Schema({
-  groupID: { type: Number, required: true },
-  members: { type: [Number], default: [] },
-});
+app.get('/api/getGroup', async (req, res) => {
+  const groupId = Math.floor(Math.random() * 100);
+  try {
+    const group = await Group.findOne({ members: groupId });
+    console.log(group, groupId);
+    res.status(200).json({group});
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+})
 
 mongoose
   .connect(mongo, {})
@@ -33,4 +50,9 @@ mongoose
   })
   .catch((err) => {
     console.log("MongoDB connection error:", err);
+    
   });
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+})
